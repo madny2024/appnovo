3861d8f55f46bc52304492ee4643debe7ab3ae31
madny2024 <criadordecena@gmail.com>
2025-07-09 15:14:10 -0300

    Update dfdfsf

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import { xtreamApi } from './api/xtreamApi';
 import authReducer from './slices/authSlice';
 import contentReducer from './slices/contentSlice';
+import { loadState, saveState } from '../utils/localStorage';
 
-// A LÓGICA DE LOCALSTORAGE FOI REMOVIDA PARA ESTE TESTE
+// Reativando o carregamento do estado salvo
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
@@ -13,4 +15,11 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(xtreamApi.middleware),
+  // Reativando o estado pre-carregado
+  preloadedState: { auth: preloadedState },
+});
+
+// Reativando o salvamento automático do estado de login
+store.subscribe(() => {
+  saveState(store.getState().auth);
 });
